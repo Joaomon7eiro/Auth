0acc78e582740571d0f2637d4774e95cd00029fa
joao monteiro <joaoagrom@gmail.com>
2019-02-10 22:27:59 -0200

    handling log out

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header } from './components/common'
-import LoginForm from './components/LoginForm'
-import firebase from '@firebase/app'
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
+import firebase from '@firebase/app';
+require('firebase/auth');
 
 class App extends Component{
-  
+  state = {
+    loggedIn: null
+  }
+
   componentWillMount(){
     firebase.initializeApp({
       apiKey: "AIzaSyB2BZ6qXQogzHuQLnToPtlZBqJjWe2AjOg",
@@ -14,14 +18,41 @@ class App extends Component{
       projectId: "auth-b81ba",
       storageBucket: "auth-b81ba.appspot.com",
       messagingSenderId: "341049403833"
-    })
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user){
+        this.setState({loggedIn: true})
+      } else {
+        this.setState({loggedIn: false})
+      }
+    });
+
+  }
+
+  renderContent(){
+    switch(this.state.loggedIn){
+      case true: 
+        return (
+          <View style={{flexDirection: 'row'}}>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          </View>
+        )
+        
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size='large'/>
+    }
   }
 
   render(){
     return(
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
